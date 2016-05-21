f2bb6c889a2156b0a4cd4d2692a7c921537e9661
Max Murphy-Skvorzov <mmurphy@telemetry.com>
2016-05-21 22:25:57 +0100

    simplify

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 /* A->0, B->1, Z->25, AA->26 etc. */
 function col2num(s){ var ans = 0; for (var i=0; i<s.length; i++) ans = ans*26+s.charCodeAt(i)-64; return ans - 1;}
 
-/* colum index -> col label strlen e.g. AA == 26 -> 2 */
-function digits(x){return Math.floor(Math.log((x+1)*25+1)/Math.log(26));}
-
 /* 0 -> A, 25 -> Z, 26 -> AA ... */
-function num2col(n){ var ans = new Array(digits(n)).fill(0); n -= ((Math.pow(26,ans.length)-1)/25) - 1; for(var i=ans.length; i--;){ ans[i] = n%26; n = (n - (n%26))/26; } return ans.map(function(n){ return String.fromCharCode(n+65); }).join(''); }
+function num2col(n){ var ans = ''; for(n++;n-->0;n=(n-(n%26))/26) ans = String.fromCharCode((n%26)+65)+ans; return ans; }
 
 /* A4 -> [0, 3, 'A4'] -- Note that the 1-up row count in the label is changed to a 0-up index. */
 function parseCell(s){ var m = s.match(/([A-Z]+)([0-9]+)/); if (m !== null) return [col2num(m[1]),Number(m[2])-1,m[0]]; }
